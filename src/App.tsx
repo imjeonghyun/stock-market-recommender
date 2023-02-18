75e8a5167ed352d6b286d6e2febff5327c5c280c
Junghyun Seo <imjeonghyun@gmail.com>
2023-02-18 14:04:09 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>first commit</header>
+    </div>
+  );
+}
+
+export default App;
